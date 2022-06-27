2ac4dd60dc3f1130e22dcf57a50ee6163cebbbfe
nenadpin <nenadpin@yahoo.com>
2022-06-27 18:50:33 +0200

    ispravke zbog pravila igre koja nisam znao detaljno

--- index.js
+++ index.js
@@ -1,135 +1,222 @@
-let prvaKarta = 0
-let drugaKarta = 0
-let dilerKarte = [0]
-let dilerSum = 0
-let karte = [0, 0]
-let sum = prvaKarta + drugaKarta
-let trecaKarta = 0
-let dobitak = false
-let igra = false
-let info = ""
+let prvaKarta = 0;
+let drugaKarta = 0;
+let dilerKarte = [];
+let dilerSum = 0;
+let diler = {};
+let karte = [];
+let sum = prvaKarta + drugaKarta;
+let trecaKarta = 0;
+let dobitak = false;
+let igra = false;
+let info = "";
 let player = {
-    name: "Nenad",
-    kredit: 100
-}
-prikaz1()
+  name: "Nenad",
+  kredit: 100,
+};
+let deck = [];
+start();
 
-function prikaz1() {
-    document.getElementById("kreni").style.display = "block"
-    document.getElementById("igra").style.display = "none"
+function start() {
+  document.getElementById("kreni").style.display = "block";
+  document.getElementById("igra").style.display = "none";
 }
-function prikaz2() {
-    document.getElementById("kreni").style.display = "none"
-    document.getElementById("igra").style.display = "block"
+function gameOn() {
+  document.getElementById("kreni").style.display = "none";
+  document.getElementById("igra").style.display = "block";
 }
+const convert = (x) => {
+  if (x === 12) {
+    return {
+      card: "J",
+      value: 10,
+      suit: "",
+    };
+  } else if (x === 13) {
+    return {
+      card: "Q",
+      value: 10,
+      suit: "",
+    };
+  } else if (x === 14) {
+    return {
+      card: "K",
+      value: 10,
+      suit: "",
+    };
+  } else if (x === 11) {
+    return {
+      card: "A",
+      value: 11,
+      suit: "",
+    };
+  } else {
+    return {
+      card: x,
+      value: x,
+      suit: "",
+    };
+  }
+};
 
-let dilerEl = document.getElementById("diler-el")
-let poruka = document.getElementById("poruka-el")
-let sumEl = document.getElementById("sum-el")
-let karteEl = document.getElementById("karte-el")
-let playerEl = document.getElementById("player-el")
-let startBtn = document.getElementById("start-Btn")
-let novaBtn = document.getElementById("nova-Btn")
-let proveraBtn = document.getElementById("provera-Btn")
+let dilerEl = document.getElementById("diler-el");
+let poruka = document.getElementById("poruka-el");
+let sumEl = document.getElementById("sum-el");
+let karteEl = document.getElementById("karte-el");
+let playerEl = document.getElementById("player-el");
+let startBtn = document.getElementById("start-Btn");
+let novaBtn = document.getElementById("nova-Btn");
+let proveraBtn = document.getElementById("provera-Btn");
 
 if (JSON.parse(localStorage.getItem("kredit"))) {
-    player.kredit = JSON.parse(localStorage.getItem("kredit"))
+  player.kredit = JSON.parse(localStorage.getItem("kredit"));
 }
 
-playerEl.textContent = "Igrac: " + player.name + "  - " + " $:" + player.kredit
-function getKard() {
-    let i = Math.floor(Math.random() * 13) + 1;
-    if (i > 10){
-        return 10
-    } else if (i===1) {
-            return 11
-    }    else {
-            return i
-        }
-}
-novaBtn.addEventListener("click", function () {
-    if (igra===true && dobitak === false) {
-    trecaKarta = getKard()
-    sum += trecaKarta
-    karte.push(trecaKarta)
-    vuci()
+playerEl.textContent = `Igrac: ${player.name}  $: ${player.kredit}`;
+
+const populateDeck = () => {
+  for (let j = 2; j < 15; j++) {
+    deck[j - 2] = convert(j);
+    deck[j - 2].suit = "Clubs";
+  }
+  for (let j = 2; j < 15; j++) {
+    deck[j + 13 - 2] = convert(j);
+    deck[j + 13 - 2].suit = "Spades";
+  }
+  for (let j = 2; j < 15; j++) {
+    deck[j + 26 - 2] = convert(j);
+    deck[j + 26 - 2].suit = "Hearts";
+  }
+  for (let j = 2; j < 15; j++) {
+    deck[j + 39 - 2] = convert(j);
+    deck[j + 39 - 2].suit = "Diamonds";
+  }
+};
+
+populateDeck();
+
+novaBtn.addEventListener("click", () => {
+  if (igra === true && dobitak === false) {
+    if (deck.length === 0) populateDeck();
+    trecaKarta = deck.splice(
+      Math.floor(Math.random() * (deck.length - 1)),
+      1
+    )[0];
+    karte.push(trecaKarta.card);
+    sum += trecaKarta.value;
+    vuci();
     karteEl.textContent += " " + karte[karte.length - 1];
-    }
-}) 
-startBtn.addEventListener("click", function () {
-    igra = true
-    dobitak = false
-    dilerKarte= [getKard()]
-    prvaKarta = getKard()
-    drugaKarta = getKard()
-    sum = prvaKarta + drugaKarta
-    karte = [prvaKarta, drugaKarta]
-    karteEl.textContent = "Karte: " + karte[0] + " " + karte[1]
-    dilerEl.textContent = "Diler: " + dilerKarte
-    vuci()
-})
-function vuci() {
-    if (igra) {
-        prikaz2()
+  }
+});
+startBtn.addEventListener("click", () => {
+  igra = true;
+  dobitak = false;
+  karte = [];
+  dilerKarte = [];
+  if (deck.length === 0) populateDeck();
+  diler = deck.splice(Math.floor(Math.random() * (deck.length - 1)), 1)[0];
+  dilerKarte[0] = diler.card;
+  dilerSum = diler.value;
+  if (deck.length === 0) populateDeck();
+  prvaKarta = deck.splice(Math.floor(Math.random() * (deck.length - 1)), 1)[0];
+  if (deck.length === 0) populateDeck();
+  drugaKarta = deck.splice(Math.floor(Math.random() * (deck.length - 1)), 1)[0];
+  karte[0] = prvaKarta.card;
+  karte[1] = drugaKarta.card;
+  sum = prvaKarta.value + drugaKarta.value;
+  karteEl.textContent = `Karte: ${karte[0]} ${karte[1]}`;
+  dilerEl.textContent = `Diler: ${dilerKarte}`;
+  vuci();
+});
+const vuci = () => {
+  if (igra) {
+    gameOn();
+  } else {
+    start();
+  }
+
+  if (sum <= 20) {
+    info = "Jos jednu kartu?";
+  } else if (sum === 21) {
+    info = "Dobili ste BlackJack!";
+    dobitak = true;
+    igra = false;
+    player.kredit += 10;
+    localStorage.setItem("kredit", JSON.stringify(player.kredit));
+    playerEl.textContent = `Igrac: ${player.name}  $: ${player.kredit}`;
+    start();
+  } else {
+    info = "Izgubili ste!";
+    igra = false;
+    player.kredit -= 10;
+    localStorage.setItem("kredit", JSON.stringify(player.kredit));
+    playerEl.textContent = `Igrac: ${player.name}  $: ${player.kredit}`;
+    start();
+  }
+  poruka.textContent = info;
+  sumEl.textContent = "Ukupno: " + sum;
+};
+
+proveraBtn.addEventListener("click", () => {
+  if (igra) {
+    if (sum === 21) {
+      poruka.textContent = "Dobili ste!!!";
+      dobitak = true;
+      player.kredit += 10;
+
+      localStorage.setItem("kredit", JSON.stringify(player.kredit));
+      playerEl.textContent = `Igrac: ${player.name}  $: ${player.kredit}`;
+      igra = false;
+      start();
     } else {
-        prikaz1()
-    }
-    
-    if (sum<=20) {
-        info = "Jos jednu kartu?"
-    } else if (sum === 21) {
-        info = "Dobili ste BlackJack!"
-        dobitak = true
-        igra=false
-        player.kredit +=10
-        localStorage.setItem("kredit", JSON.stringify(player.kredit))
-        playerEl.textContent = "Igrac: " + player.name + "  - " + " $:" + player.kredit
-        prikaz1()
-    } else {
-        info = "Izgubili ste!"
-        igra = false
-        player.kredit -=10
-        localStorage.setItem("kredit", JSON.stringify(player.kredit))
-        playerEl.textContent = "Igrac: " + player.name + "  - " + " $:" + player.kredit
-        prikaz1()
-    }
-poruka.textContent = info;
-sumEl.textContent = "Ukupno: " + sum;
-}
-proveraBtn.addEventListener("click", function () {
-if(igra === true) {
-    if (sum===21) {
-        poruka.textContent = "Dobili ste!!!"
-        dobitak = true
-        igra=false
-        player.kredit += 10
-        localStorage.setItem("kredit", JSON.stringify(player.kredit))
-        playerEl.textContent = "Igrac: " + player.name + "  - " + " $:" + player.kredit
-        prikaz1()
-    } else {
-    
-    dilerSum = dilerKarte[0]
-    for(let i = 1; sum > dilerSum; i++) {
-        dilerKarte.push(getKard())
-        dilerEl.textContent += " " + dilerKarte[i];
-        dilerSum += dilerKarte[i];
-    }
-    if(dilerSum > 21) {
-        poruka.textContent = "Dobili ste!!!"
-        dobitak = true
-        player.kredit += 10
-        localStorage.setItem("kredit", JSON.stringify(player.kredit))
-        playerEl.textContent = "Igrac: " + player.name + "  - " + " $:" + player.kredit
-        prikaz1()
-        igra=false
-    } else {
-        poruka.textContent = "Izgubili ste!!!"
-        player.kredit -= 10
-        localStorage.setItem("kredit", JSON.stringify(player.kredit))
-        playerEl.textContent = "Igrac: " + player.name + "  - " + " $:" + player.kredit
-        prikaz1()
-        igra = false
+      while (igra) {
+        if (deck.length === 0) populateDeck();
+        diler = deck.splice(
+          Math.floor(Math.random() * (deck.length - 1)),
+          1
+        )[0];
+        dilerKarte.push(diler.card);
+        dilerEl.textContent += " " + diler.card;
+        dilerSum += diler.value;
+        if (dilerSum > 21) {
+          poruka.textContent = "Dobili ste!!!";
+          dobitak = true;
+          player.kredit += 10;
+          localStorage.setItem("kredit", JSON.stringify(player.kredit));
+
+          playerEl.textContent = `Igrac: ${player.name}  $: ${player.kredit}`;
+          igra = false;
+          start();
+          break;
         }
-}
-}
-})
+        if (dilerSum >= 17) {
+          if (sum === dilerSum) {
+            poruka.textContent = "Nereseno!";
+            igra = false;
+            start();
+            break;
+          } else if (sum > dilerSum) {
+            poruka.textContent = "Dobili ste!!!";
+            dobitak = true;
+            player.kredit += 10;
+            localStorage.setItem("kredit", JSON.stringify(player.kredit));
+
+            playerEl.textContent = `Igrac: ${player.name}  $: ${player.kredit}`;
+            igra = false;
+            start();
+            break;
+          } else {
+            poruka.textContent = "Izgubili ste!!!";
+            dobitak = false;
+            player.kredit -= 10;
+            localStorage.setItem("kredit", JSON.stringify(player.kredit));
+
+            playerEl.textContent = `Igrac: ${layer.name}  $: ${player.kredit}`;
+            igra = false;
+            start();
+            break;
+          }
+        }
+      }
+    }
+  }
+});
